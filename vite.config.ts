e99b6f07f77ebe7f325d66eeb8a46fccbd346c26
PericaGlisa <petarglisovic@esamurai.net>
2025-05-14 18:06:55 +0200

    Fix white screen issue on Netlify deployment by updating Vite config and index.html

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/",
   server: {
     host: "::",
     port: 8080,
@@ -15,4 +16,10 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+    sourcemap: false,
+    minify: true
+  }
 }));
